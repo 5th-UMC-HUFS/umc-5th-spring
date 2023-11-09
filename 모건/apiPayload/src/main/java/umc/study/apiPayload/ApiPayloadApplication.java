177aaf4363be5248883e192ccd5e831588c977db
구환준/모건 <koojun99@naver.com>
2023-11-09 10:02:24 +0900

    initial commit

---
+++ 모건/apiPayload/src/main/java/umc/study/apiPayload/ApiPayloadApplication.java
@@ -0,0 +1,13 @@
+package umc.study.apiPayload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPayloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPayloadApplication.class, args);
+	}
+
+}
---
+++ 모건/apiPayload/src/test/java/umc/study/apiPayload/ApiPayloadApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.study.apiPayload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPayloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
